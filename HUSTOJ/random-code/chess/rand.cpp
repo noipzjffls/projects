f5ca305b1da08327e151ab5dc6ba796fd13d1ece
noipzjffls <ericoder@qq.com>
2020-04-05 09:49:56 +0800

    Update rand.cpp

--- HUSTOJ/random-code/chess/rand.cpp
+++ HUSTOJ/random-code/chess/rand.cpp
@@ -1,25 +1,34 @@
 #include<bits/stdc++.h>
 bool a[1001][1001];
-int m,n,x,y,z;
+int type[]={0,0,0,0,1,1,1,2,2,2,2},datanum,rangen[][2]={{1,10},{1,200},{1,1000}},rangem[][2]={{1,5},{1,20},{1,100}};
+int random(int a,int b)
+{
+    return (rand()%(b-a+1))+a;
+}
+char filename[][10]={"","1.in","2.in","3.in","4.in","5.in","6.in","7.in","8.in","9.in","10.in"};
 int main()
 {
-    srand(unsigned(time(NULL)));
-    m=rand()%100+1;
-    n=rand()%1000+1;
-    printf("%d %d",m,n);
-    for(int i=0;i<n;i++)
+    srand((time(NULL)));
+    for(datanum=9;datanum<=9;datanum++)
     {
-        while(1)
+        freopen(filename[datanum],"w",stdout);
+        memset(a,0,sizeof(a));
+        int typenum=type[datanum],m=random(rangem[typenum][0],rangem[typenum][1]),n=random(rangen[typenum][0],rangen[typenum][1]),x,y,z;
+        printf("%d %d",m,n);
+        while(n--)
         {
-            x=rand()%m+1;
-            y=rand()%m+1;
-            z=rand()%2;
-            if(!a[x][y])
+            z=random(0,1);
+            while(1)
             {
-                a[x][y]=1;
-                break;
+                x=random(1,m);
+                y=random(1,m);
+                if(!a[x][y])
+                {
+                    a[x][y]=1;
+                    break;
+                }
             }
+            printf("\n%d %d %d",x,y,z);
         }
-        printf("\n%d %d %d",x,y,z);
     }
 }
