fc751abe3a6a6a67a6b381a56fb8017c2ee59892
Eric <ericshen4@yeah.net>
2020-04-25 17:58:03 +0800

    Create 2.cpp

---
+++ luogu/P6387/2.cpp
@@ -0,0 +1,45 @@
+#include<cstdio>
+#include<cmath>
+int x,a[10],b[10];
+int pow10(int x)
+{
+    int s=1;
+    while(x--)s=(s<<3)+(s<<1);
+    return s;
+}
+void f(int n,int check)
+{
+    int s=0;
+    while(n)
+    {
+        if(check==1)b[n%10]++;
+        else if(!check)a[n%10]++;
+        s++;
+        n/=10;
+    }
+}
+bool check()
+{
+    for(int i=0;i<10;i++)if(a[i]!=b[i])return 0;
+    return 1;
+}
+void memset()
+{
+    for(int i=0;i<10;i++)b[i]=0;
+}
+int main()
+{
+    scanf("%d",&x);
+    f(x,0);
+    for(int i=x+1;i<pow10(ceil(log10(x)));i++)
+    {
+        memset();
+        f(i,1);
+        if(check())
+        {
+            printf("%d",i);
+            return 0;
+        }
+    }
+    putchar('0');
+}
