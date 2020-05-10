b7731577e4ecae2b4fdf6a014e6fa95ab2efe3f6
Eric <ericshen4@yeah.net>
2020-05-10 16:12:12 +0800

    Documents of CF290B updated.

---
+++ luogu/CF290B/chart.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    freopen("qr-code.txt","r",stdin);
+    string s;
+    while(cin>>s)
+    {
+        cout<<"\""<<s<<"\",";
+    }
+}
