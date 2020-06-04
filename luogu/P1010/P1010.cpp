d3abd961ade803e2f213fe560b57d3329493f692
shenyouran <60684241+shenyouran@users.noreply.github.com>
2020-06-04 12:17:22 +0800

    Create P1010.cpp

---
+++ luogu/P1010/P1010.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+bool flag;
+string f[20001]={"0","2(0)","2"};
+string ans(int x)
+{
+    flag=false;
+    if(f[x]!="")
+    {
+        if(!flag)
+        {
+            flag=true;
+            return f[x];
+        }
+        else return "+"+f[x];
+    }
+    int p=log2(x),q=1<<p;
+    if(q==x)
+    {
+        if(p==1)return "2";
+        return f[x]="2("+ans(p)+")";
+    }
+    return f[x]=ans(q)+'+'+ans(x-q);
+}
+int main()
+{
+	scanf("%d",&n);
+	cout<<ans(n);
+    return 0;
+}
