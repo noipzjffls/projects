02f748f1236fc12d8e765f424c1c2b23b202031c
Eric <ericshen4@yeah.net>
2020-06-07 08:50:41 +0800

    Create P3368.cpp

---
+++ luogu/P3368/P3368.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#define lowbit(x) x&-x
+using namespace std;
+int n,m,a[500001],c[500001];
+void update(int x,int d)
+{
+    while(x<=n)
+    {
+        c[x]+=d;
+        x+=lowbit(x);
+    }
+}
+int sum(int x)
+{
+    int s=0;
+    while(x>0)
+    {
+        s+=c[x];
+        x-=lowbit(x);
+    }
+    return s;
+}
+int read()
+{
+    int x=0,f=1;
+    char ch=getchar();
+    while(ch<'0'||ch>'9')
+    {
+        if(ch=='-')f=-1;
+        ch=getchar();
+    }
+    while(ch>='0'&&ch<='9')
+    {
+        x=(x<<1)+(x<<3)+(ch^48);
+        ch=getchar();
+    }
+    return x*f;
+}
+int main()
+{
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+        update(i,a[i]-a[i-1]);
+    }
+    while(m--)
+    {
+        int opt=read();
+        if(opt==1)
+        {
+            int x=read(),y=read(),k=read();
+            update(x,k);
+            update(y+1,-k);
+        }
+        else cout<<sum(read())<<endl;
+    }
+    return 0;
+}
