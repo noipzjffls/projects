512d47a4d842580b1df88df7f00d4832c2d4950d
Eric <ericshen4@yeah.net>
2020-06-15 22:00:15 +0800

    Create map.cpp

---
+++ luogu/P2249/map.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m;
+map<int,int>M;
+int read()
+{
+    int x=0,f=1;
+    char ch=getchar();
+    while(ch<'0'||ch>'9')
+    {
+        if(ch=='-')f=-1;
+        ch=getchar();
+    }
+    while(ch>='0'&&ch<='9')
+    {
+        x=(x<<1)+(x<<3)+(ch^48);
+        ch=getchar();
+    }
+    return x*f;
+}
+int main()
+{
+    n=read();
+    m=read();
+    for(int i=1,x;i<=n;i++)
+    {
+        x=read();
+        if(M[x])continue;
+        M[x]=i;
+    }
+    while(m--)
+    {
+        int x=read();
+        printf("%d ",!M[x]?-1:M[x]);
+    }
+    return 0;
+}
