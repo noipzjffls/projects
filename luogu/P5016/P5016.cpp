164cb7a6a509c5a242702b71804d432172b9ec8b
Eric <ericshen4@yeah.net>
2020-08-01 18:16:30 +0800

    Update P5016.cpp

--- luogu/P5016/P5016.cpp
+++ luogu/P5016/P5016.cpp
@@ -1,22 +1,17 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
 int n,m,p1,s1,s2,ans,c[100001];
 long long a,b,mins,start;
 int read()
 {
-    int x=0,f=1;
+    int x=0;
     char ch=getchar();
-    while(ch<'0'||ch>'9')
-    {
-        if(ch=='-')f=-1;
-        ch=getchar();
-    }
+    while(ch<'0'||ch>'9')ch=getchar();
     while(ch>='0'&&ch<='9')
     {
         x=(x<<1)+(x<<3)+(ch^48);
         ch=getchar();
     }
-    return x*f;
+    return x;
 }
 int main()
 {
@@ -24,15 +19,15 @@ int main()
     for(int i=1;i<=n;i++)c[i]=read();
     m=read(),p1=read(),s1=read(),s2=read();
     c[p1]+=s1;
-    for(int i=1;i<m;i++)a+=1LL*(m-i)*c[i];
-    for(int i=m+1;i<=n;i++)b+=1LL*(i-m)*c[i];
+    for(int i=1;i<m;i++)a+=(long long)(m-i)*c[i];
+    for(int i=m+1;i<=n;i++)b+=(long long)(i-m)*c[i];
     ans=m;
     start=a-b;
     mins=llabs(start);
-    for(int i=1;i<m;i++)
+    for(int i=m-1;i;i--)
     {
-        long long cur=llabs(start+1LL*(m-i)*s2);
-        if(cur<mins)
+        long long cur=llabs(start+(long long)(m-i)*s2);
+        if(cur<=mins)
         {
             mins=cur;
             ans=i;
@@ -40,7 +35,7 @@ int main()
     }
     for(int i=m+1;i<=n;i++)
     {
-        long long cur=llabs(start-1LL*(i-m)*s2);
+        long long cur=llabs(start-(long long)(i-m)*s2);
         if(cur<mins)
         {
             mins=cur;
