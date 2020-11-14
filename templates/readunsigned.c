d43dc7826515aa8f366e5e285180268a3ebbe7f9
Eric <ericshen4@yeah.net>
2020-11-14 16:00:49 +0800

    New read-integer template used for unsigned integers.

---
+++ templates/readunsigned.c
@@ -0,0 +1,12 @@
+int read()
+{
+    int x=0;
+    char ch=getchar();
+    while(ch<'0'||ch>'9')ch=getchar();
+    while(ch>='0'&&ch<='9')
+    {
+        x=(x<<1)+(x<<3)+(ch^48);
+        ch=getchar();
+    }
+    return x;
+}
